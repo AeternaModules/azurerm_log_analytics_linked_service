dcbb0ff46ae08d41b9e58d3f8acecedf84116acc
Daniel Allen <dan@aeternagroup.net>
2026-01-11 15:40:37 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "log_analytics_linked_services" {
+  description = "All log_analytics_linked_service resources"
+  value       = azurerm_log_analytics_linked_service.log_analytics_linked_services
+}
+
