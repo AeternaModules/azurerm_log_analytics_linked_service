e682624f98d1b68a76e638895dd0d212d8cd7354
Daniel Allen <dan@aeternagroup.net>
2026-07-06 23:13:21 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -2,4 +2,24 @@ output "log_analytics_linked_services" {
   description = "All log_analytics_linked_service resources"
   value       = azurerm_log_analytics_linked_service.log_analytics_linked_services
 }
+output "log_analytics_linked_services_name" {
+  description = "List of name values across all log_analytics_linked_services"
+  value       = [for k, v in azurerm_log_analytics_linked_service.log_analytics_linked_services : v.name]
+}
+output "log_analytics_linked_services_read_access_id" {
+  description = "List of read_access_id values across all log_analytics_linked_services"
+  value       = [for k, v in azurerm_log_analytics_linked_service.log_analytics_linked_services : v.read_access_id]
+}
+output "log_analytics_linked_services_resource_group_name" {
+  description = "List of resource_group_name values across all log_analytics_linked_services"
+  value       = [for k, v in azurerm_log_analytics_linked_service.log_analytics_linked_services : v.resource_group_name]
+}
+output "log_analytics_linked_services_workspace_id" {
+  description = "List of workspace_id values across all log_analytics_linked_services"
+  value       = [for k, v in azurerm_log_analytics_linked_service.log_analytics_linked_services : v.workspace_id]
+}
+output "log_analytics_linked_services_write_access_id" {
+  description = "List of write_access_id values across all log_analytics_linked_services"
+  value       = [for k, v in azurerm_log_analytics_linked_service.log_analytics_linked_services : v.write_access_id]
+}
 
