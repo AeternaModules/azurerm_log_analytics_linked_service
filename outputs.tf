c0536069f8a2be9a0c5f0a371acc8645586a61b6
Daniel Allen <dan@aeternagroup.net>
2026-07-16 14:48:51 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,25 +1,25 @@
 output "log_analytics_linked_services_id" {
   description = "Map of id values across all log_analytics_linked_services, keyed the same as var.log_analytics_linked_services"
-  value       = { for k, v in azurerm_log_analytics_linked_service.log_analytics_linked_services : k => v.id }
+  value       = { for k, v in azurerm_log_analytics_linked_service.log_analytics_linked_services : k => v.id if v.id != null && length(v.id) > 0 }
 }
 output "log_analytics_linked_services_name" {
   description = "Map of name values across all log_analytics_linked_services, keyed the same as var.log_analytics_linked_services"
-  value       = { for k, v in azurerm_log_analytics_linked_service.log_analytics_linked_services : k => v.name }
+  value       = { for k, v in azurerm_log_analytics_linked_service.log_analytics_linked_services : k => v.name if v.name != null && length(v.name) > 0 }
 }
 output "log_analytics_linked_services_read_access_id" {
   description = "Map of read_access_id values across all log_analytics_linked_services, keyed the same as var.log_analytics_linked_services"
-  value       = { for k, v in azurerm_log_analytics_linked_service.log_analytics_linked_services : k => v.read_access_id }
+  value       = { for k, v in azurerm_log_analytics_linked_service.log_analytics_linked_services : k => v.read_access_id if v.read_access_id != null && length(v.read_access_id) > 0 }
 }
 output "log_analytics_linked_services_resource_group_name" {
   description = "Map of resource_group_name values across all log_analytics_linked_services, keyed the same as var.log_analytics_linked_services"
-  value       = { for k, v in azurerm_log_analytics_linked_service.log_analytics_linked_services : k => v.resource_group_name }
+  value       = { for k, v in azurerm_log_analytics_linked_service.log_analytics_linked_services : k => v.resource_group_name if v.resource_group_name != null && length(v.resource_group_name) > 0 }
 }
 output "log_analytics_linked_services_workspace_id" {
   description = "Map of workspace_id values across all log_analytics_linked_services, keyed the same as var.log_analytics_linked_services"
-  value       = { for k, v in azurerm_log_analytics_linked_service.log_analytics_linked_services : k => v.workspace_id }
+  value       = { for k, v in azurerm_log_analytics_linked_service.log_analytics_linked_services : k => v.workspace_id if v.workspace_id != null && length(v.workspace_id) > 0 }
 }
 output "log_analytics_linked_services_write_access_id" {
   description = "Map of write_access_id values across all log_analytics_linked_services, keyed the same as var.log_analytics_linked_services"
-  value       = { for k, v in azurerm_log_analytics_linked_service.log_analytics_linked_services : k => v.write_access_id }
+  value       = { for k, v in azurerm_log_analytics_linked_service.log_analytics_linked_services : k => v.write_access_id if v.write_access_id != null && length(v.write_access_id) > 0 }
 }
 
